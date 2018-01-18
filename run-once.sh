3b3e378dd839d26e518a40c6e49bf2aa3c2f0801
Lazar Obradovic <laz.obradovic@gmail.com>
2018-01-18 21:44:27 +0100

    check for root and docker.

--- run-once.sh
+++ run-once.sh
@@ -2,8 +2,13 @@
 #
 # Run this script once on your build hosts
 
-if [ ! -x $(which docker) ]; then
-  echo ERROR: No docker installer.
+if [[ ${EUID} -ne 0 ]]; then
+  echo ERROR: This script should be run as root. 1>&2
+  exit 1
+fi
+
+if [[ ! $(type -P docker) ]]; then
+  echo ERROR: No docker installer. 1>&2
   exit 1
 fi
 
@@ -18,7 +23,7 @@ fi
 make -f docker.Makefile cross
 export PATH=${PATH}:$(pwd)/build
 
-echo "INFO: You should add $(pwd)/build to your \$PATH"
+echo "INFO: You should permanently add $(pwd)/build to your \$PATH"
 
 echo "INFO: Registering handlers"
 if [ $(uname -s) != "Darwin" ]; then
