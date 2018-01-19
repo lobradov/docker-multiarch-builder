eb3d66aec9e8155fd15f992530d7449846d96fe9
Lazar Obradovic <laz.obradovic@gmail.com>
2018-01-19 01:37:00 +0100

    don't require root, but do sudo.

--- run-once.sh
+++ run-once.sh
@@ -2,30 +2,35 @@
 #
 # Run this script once on your build hosts
 
-if [[ ${EUID} -ne 0 ]]; then
-  echo ERROR: This script should be run as root. 1>&2
-  exit 1
-fi
+set -e
 
 if [[ ! $(type -P docker) ]]; then
   echo ERROR: No docker installer. 1>&2
   exit 1
 fi
 
+if [[ ! $(type -P make) ]]; then
+  echo ERROR: No docker installer. 1>&2
+  exit 1
+fi
+
+
 echo "INFO: Getting new docker CLI"
 if [ ! -d cli ]; then
-  git clone -b manifest-cmd https://github.com/clnperez/cli.git
+  git clone -b manifest-cmd https://github.com/clnperez/cli.git || \
+    echo ERROR: Could not clone the repository 1>&2 &&
   cd cli
 else
   cd cli
   git pull
 fi
 make -f docker.Makefile cross
+
 export PATH=${PATH}:$(pwd)/build
-
 echo "INFO: You should permanently add $(pwd)/build to your \$PATH"
+echo "$ echo export PATH=${PATH}:$(pwd)/build >> ~/.profile"
 
-echo "INFO: Registering handlers"
 if [ $(uname -s) != "Darwin" ]; then
-  docker run --rm --privileged multiarch/qemu-user-static:register
+  echo "INFO: Registering handlers - requires sudo!"
+  sudo docker run --rm --privileged multiarch/qemu-user-static:register
 fi
